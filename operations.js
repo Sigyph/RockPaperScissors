af81c6354ca98d68eafc42bdfeb3cb4847e160e4
Sigyph <145731650+Sigyph@users.noreply.github.com>
2026-05-18 23:39:12 -0600

    Add computer choice for the 3 options

--- operations.js
+++ operations.js
@@ -1,1 +1,18 @@
-console.log("Hello world!");
+function getComputerChoice(){
+    let choice;
+    let randomvalue = Math.floor((Math.random())*3);
+    switch (randomvalue){
+        case 0:
+            choice="Rock";
+            break;
+        case 1:
+            choice="Paper";
+            break;
+        case 2:
+            choice="Scissor";
+            break;
+    }
+    return choice;
+}
+
+console.log(getComputerChoice());
