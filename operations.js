d4ffe32e6a55bb35878b6ab6a466ab71692fcd6e
Sigyph <145731650+Sigyph@users.noreply.github.com>
2026-05-19 00:13:20 -0600

    Add code to play a round

--- operations.js
+++ operations.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     let choice;
-    let randomvalue = Math.floor((Math.random()) * 3);
+    const randomvalue = Math.floor((Math.random()) * 3);
     switch (randomvalue) {
         case 0:
             choice = "Rock";
@@ -14,14 +14,73 @@ function getComputerChoice() {
     }
     return choice;
 }
-
 function getHumanChoice() {
-    let user = prompt("Choose rock, paper or scissors");
+    const user = prompt("Choose rock, paper or scissors");
     return user.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    let result;
+
+    if (computerChoice === "Rock") {
+        switch (humanChoice) {
+            case "rock":
+                console.log("Tie!" + "Computer chose " + computerChoice);
+                break;
+            case "scissors":
+                console.log("You lose! " + "Computer chose " + computerChoice);
+                computerScore++;
+                break;
+            case "paper":
+                console.log("You win! " + "Computer chose " + computerChoice);
+                humanScore++;
+                break;
+            default:
+                console.log("Your text has errors");
+        }
+    }
+    else if (computerChoice === "Paper") {
+        switch (humanChoice) {
+            case "paper":
+                console.log("Tie!" + "Computer chose " + computerChoice);
+                break;
+            case "rock":
+                console.log("You lose! " + "Computer chose " + computerChoice);
+                computerScore++;
+                break;
+            case "scissors":
+                console.log("You win! " + "Computer chose " + computerChoice);
+                humanScore++;
+                break;
+            default:
+                console.log("Your text has errors");
+        }
+    }
+    else {
+        switch (humanChoice) {
+            case "scissors":
+                console.log("Tie!" + "Computer chose " + computerChoice);
+                break;
+            case "paper":
+                console.log("You lose! " + "Computer chose " + computerChoice);
+                computerScore++;
+                break;
+            case "rock":
+                console.log("You win! " + "Computer chose " + computerChoice);
+                humanScore++;
+                break;
+            default:
+                console.log("Your text has errors");
+        }
+    }
+    console.log("Current scores => Computer: "+computerScore+"  "+ "User: "+humanScore);
+}
+
 
 let humanScore = 0;
 let computerScore = 0;
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
